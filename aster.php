e5fdb7bfbf1197303491b2ccb3787e3a1aad6c5a
Reidoo92 <119537150+Reidoo92@users.noreply.github.com>
2024-06-20 16:12:43 +0200

    création de la class aster

--- aster.php
+++ aster.php
@@ -2,8 +2,21 @@
 
 class Aster
 {
+    public $name;
+    public $mass;
+    public $diameter;
+    public $speed;
+    public $gravity;
 
-    public function __construct(){
-        echo 'Laïd le bg';
+    public function __construct($name, $mass, $diameter, $speed, $gravity){
+        $this->name = $name;
+        $this->mass = $mass;
+        $this->diameter = $diameter;
+        $this->speed = $speed;
+        $this->gravity = $gravity;
+    }
+
+    public function showMass(){
+        echo $this->name . 'à une masse de ' . $this->mass;
     }
 }
