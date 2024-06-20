bd1360d0e7c3939cd61540bf1892e37771204112
julienmattei <75244399+julienmattei@users.noreply.github.com>
2024-06-20 17:02:01 +0200

    maj satellite.php
    ajout de toString

--- satellite.php
+++ satellite.php
@@ -6,6 +6,15 @@ class Satellite extends Planet
     public function __construct(bool $visite) {
         $this->visite = $visite;
     }
+
+    public function __toString() {
+        if($this->visite) {
+            return $this->name. "a été visité";
+        } else {
+            return $this->name. "n'a pas été visité";
+        }
+    }
+    
 }
 
 $lune = new Satellite(True);
