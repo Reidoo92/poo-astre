81d61511d08330cfab130a02893d2c97b9b02b40
julienmattei <75244399+julienmattei@users.noreply.github.com>
2024-06-20 16:21:42 +0200

    maj star.php
    maj du fichier star.php

---
+++ star.php
@@ -0,0 +1,11 @@
+<?php
+
+class Star extends Aster 
+{
+    public $temperature = "";
+    public $solarMass = "";
+    public function __construct($temperature, $solarMass) {
+        $this-> temperature;
+        $this-> solarMass;
+    }
+}
